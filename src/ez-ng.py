37630f1ab8ef8383abfcf0a9f25d87c3116a9017
downdechill <nyajenye@proton.me>
2026-09-14 15:05:44 -0300

    1.36

--- src/ez-ng.py
+++ src/ez-ng.py
@@ -265,6 +265,15 @@ tk.Button(
 more_frame = tk.Frame(tabs)
 tabs.add(more_frame, text="More")
 
+label = tk.Label(more_frame, text="More")
+label.pack()
+
+label = tk.Label(controls, text="Network")
+label.pack()
+
+label = tk.Label(notepad_frame, text="Notepad")
+label.pack()
+
 tk.Button(
     more_frame,
     text="tor services on",
